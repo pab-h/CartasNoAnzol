acf6dee6695962fa2052f7e461c05fbf722a9bef
pab-h <dev.pab.2020@gmail.com>
2023-07-09 09:10:42 -0300

    Fix Texto de merge gerado pelo git

--- cliente/src/main/Main.java
+++ cliente/src/main/Main.java
@@ -1,19 +1,13 @@
 package main;
 
-<<<<<<< HEAD
-import apresentacao.*;
-=======
+import apresentacao.Janela;
 import negocio.Cliente;
->>>>>>> fcf16c78f75c0105ddabbc5979b903c2e8eda7b1
 
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Janela janela = new Janela();
-=======
 		Cliente cliente = new Cliente(1313);
->>>>>>> fcf16c78f75c0105ddabbc5979b903c2e8eda7b1
 	}
 	
 }
